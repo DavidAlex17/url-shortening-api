1917f8b51f0816967b228f9314ecae62e83b9ef2
davidalex17e <davidalexx17@icloud.com>
2022-03-18 15:32:49 -0500

    Navigation functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const navMenu = document.querySelector(".nav__menu");
+const navButton = document.querySelector(".nav__button");
+const navLinks = document.querySelectorAll(".nav--link");
+const navMenuClose = document.querySelector(".menuClose");
+const navMenuOpen = document.querySelector(".menuOpen");
+
+navLinks.forEach(navLink => navLink.addEventListener("click", toggleMenu));
+
+navButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (navMenu.classList.contains("showMenu")) {
+    navMenu.classList.remove("showMenu");
+    navMenuClose.style.display = "none";
+    navMenuOpen.style.display = "block";
+  } else {
+    navMenu.classList.add("showMenu");
+    navMenuClose.style.display = "block";
+    navMenuOpen.style.display = "none";
+  }
+}
